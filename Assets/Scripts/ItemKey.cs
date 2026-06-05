3a2d24a7f9d30eaae51f4de3de2fa4457c869601
Dominic-Um <dumbrasa@ucsc.edu>
2026-06-04 23:41:51 -0700

    Key item and door + scripts

---
+++ Assets/Scripts/ItemKey.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private string keyID = "DoorKey";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+            if (inventory != null)
+            {
+                inventory.AddKey(keyID);
+                Destroy(gameObject); 
+            }
+        }
+    }
+}
