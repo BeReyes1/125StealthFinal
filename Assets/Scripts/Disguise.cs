dc997884fc865f7837ae654a2784ed9ed03f2bda
SuperYoshiStar <quickscopesuperyoshistar@gmail.com>
2026-06-07 20:30:23 -0700

    level3 implemented

--- Assets/Scripts/Disguise.cs
+++ Assets/Scripts/Disguise.cs
@@ -4,12 +4,13 @@ using TMPro;
 public class DisguiseItem : MonoBehaviour
 {
     [SerializeField] private GameObject disguiseText;
+    [SerializeField] private float disguiseTime = 30f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.GetComponent<PlayerController>().ActivateDisguise(30f);
+            other.GetComponent<PlayerController>().ActivateDisguise(disguiseTime);
             if (disguiseText != null)
                 disguiseText.SetActive(true);
             Destroy(gameObject);
